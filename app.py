1a33b8d7e135e06873a3cd7052abea2ecaf80086
Georges Ryan <gryan.ab@gmail.com>
2019-03-13 11:51:05 +0100

    app.py first commit
    - write in temporary host file websites wished to be blocked

---
+++ app.py
@@ -0,0 +1,28 @@
+import time
+from datetime import datetime as dt
+
+# On Windows
+# host_path = r'C:\Windows\System32\drivers\etc\hosts'
+
+# working with temporary file while script not complete
+host_temp = 'hosts.txt'
+# host_path = r'etc/hosts'  # On Linux and Mac
+redirect = '127.0.0.1'
+
+# list of all sites you wish to block
+blocked_websites = ['www.facebook.com', 'facebook.com', 'www.instagram.com', 'instagram.com', 'www.netflix.com']
+
+
+while True:
+    if dt(dt.now().year, dt.now().month, dt.now().day, 8) < dt.now() < dt(dt.now().year, dt.now().month, dt.now().day, 17) \
+            or dt(dt.now().year, dt.now().month, dt.now().day, 22) < dt.now() < dt(dt.now().year, dt.now().month, dt.now().day, 8):
+        with open(host_temp, 'r+') as file:
+            content = file.read()
+            for website in blocked_websites:
+                if website in content:
+                    pass
+                else:
+                    file.write(redirect + ' ' + website + '\n')
+    else:
+        print('Non office or sleeping hours')
+    time.sleep(10)
